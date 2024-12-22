a4474faa96b45bd429e6e668f78cfcce3c1e2b4f
jud-as <guilhermevalente.dev@gmail.com>
2024-12-22 17:21:35 -0300

    default-login component structure

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-default-login-layout',
-  imports: [
-    NgOptimizedImage
-
-  ],
+  imports: [],
   templateUrl: './default-login-layout.component.html',
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
-
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
 }
